4358525d79ac750b165f504ebd1ef0fe0204b881
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-10-04 22:12:06 +0100

    push

--- dev_meet_app/views.py
+++ dev_meet_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.db.models import Q
 from django.http import HttpResponse
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from django.shortcuts import get_object_or_404
 from .forms import RoomForm
 from django.contrib.auth import authenticate, login, logout
@@ -72,7 +72,20 @@ def home(request):
 def rooms(request, pk):
     context = {}
     room = get_object_or_404(Room, pk=pk)
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+
     context['room'] = room
+    context['room_messages'] = room_messages
+    context['participants'] = participants
     return render(request, 'room.html', context)
 
 @login_required(login_url='login')
@@ -116,3 +129,15 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'delete.html', {'obj':room})
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You are not allowed here!!')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request, 'delete.html', {'obj':message})
