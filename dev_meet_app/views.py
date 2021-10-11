483bb10aaf3db80d110648c950e345c4601077fd
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-10-11 03:43:00 +0100

    commit

--- dev_meet_app/views.py
+++ dev_meet_app/views.py
@@ -106,14 +106,19 @@ def userProfile(request, username):
 def createRoom(request):
     context = {}
     form = RoomForm
+    topics = Topic.objects.all()
     if request.method == 'POST':
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            return redirect('home')
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        Room.objects.create(
+            host = request.user,
+            topic = topic, 
+            name = request.POST.get('name'),
+            description = request.POST.get('description'),
+        )
+        return redirect('home')
     context['form'] = form
+    context['topics'] = topics
     return render(request, 'room_form.html', context)
 
 @login_required(login_url='login')
@@ -121,17 +126,20 @@ def updateRoom(request, pk):
     context = {}
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
-
+    topics = Topic.objects.all()
     if request.user != room.host:
         return HttpResponse('You are not allowed here!!')
 
     if request.method == 'POST':
-        form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get('name')
+        room.topic = request.POST.get('topic')
+        room.description = request.POST.get('description')
+        return redirect('home')
     context['room'] = room
     context['form'] = form
+    context['topics'] = topics
     return render(request, 'room_form.html', context)
 
 @login_required(login_url='login')
