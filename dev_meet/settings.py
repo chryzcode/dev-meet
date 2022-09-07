39fd90558f422c247b5f52254e756e306cbd2361
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-09-07 17:13:15 +0100

    PUSH

--- dev_meet/settings.py
+++ dev_meet/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         "NAME": config("DB_NAME"),
         "USER": config("DB_USER"),
         "PASSWORD": config("DB_PASSWORD"),
-        'HOST': 'localhost',
+        'HOST': config("HOST"),
         'POST': '5432',
     }
 }
