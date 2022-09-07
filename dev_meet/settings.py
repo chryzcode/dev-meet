06051a12c3c8798e002531f4bdf5a420f42aba57
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-09-07 16:57:26 +0100

    push

--- dev_meet/settings.py
+++ dev_meet/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 MEDIA_URL = '/images/'
