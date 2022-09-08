d7488713579b0fc62e24ece61be1d437c5776038
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-09-08 17:48:17 +0100

    push

--- dev_meet/settings.py
+++ dev_meet/settings.py
@@ -85,12 +85,6 @@ WSGI_APPLICATION = 'dev_meet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': {
